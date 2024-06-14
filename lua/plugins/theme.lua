0147c3ad67e08a0b4014c8b4eba3be0559731b73
Tariq <tsaleh2464@gmail.com>
2024-06-14 11:35:09 -0400

    ajusted theme and added tmux integration

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,13 +3,25 @@ return {
     "RRethy/base16-nvim",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("base16-woodland")
+      --vim.cmd.colorscheme("base16-pico")
+      --vim.cmd.colorscheme("base16-macintosh")
+      --vim.cmd.colorscheme("base16-isotope")
+      vim.cmd.colorscheme("base16-irblack")
+      --vim.cmd.colorscheme("base16-bright")
+      --vim.cmd.colorscheme("base16-brewer")
+      --vim.cmd.colorscheme("base16-grayscale-dark")
+      --vim.cmd.colorscheme("base16-default-dark")
+      --vim.cmd.colorscheme("base16-harmonic-dark")
+      --vim.cmd.colorscheme("base16-dracula")
+      --vim.cmd.colorscheme("base16-paraiso")
+      --vim.cmd.colorscheme("base16-woodland")
       --vim.cmd.colorscheme("base16-mocha")
       --vim.cmd.colorscheme("base16-marrakesh")
       --vim.cmd.colorscheme("base16-darktooth")
       --vim.cmd.colorscheme("base16-embers")
       --vim.cmd.colorscheme("base16-bespin")
       --vim.cmd.colorscheme('base16-tokyo-night-terminal-dark')
+      --vim.cmd.colorscheme("base16-gruvbox-material-dark-hard")
     end,
   },
 }
